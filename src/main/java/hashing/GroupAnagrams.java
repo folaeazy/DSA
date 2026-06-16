585e1dd1a3ee4d83c82a616f54c05b3e8854ea6a
folaeazy <folaeazy0423@gmail.com>
2026-06-16 13:35:49 +0100

    solved anagram in T:C 0(n. klogk) and O(n . k)

--- src/main/java/hashing/GroupAnagrams.java
+++ src/main/java/hashing/GroupAnagrams.java
@@ -1,20 +1,81 @@
 package hashing;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class GroupAnagrams {
 
     public static void main(String[] args) {
         String[] str = {"bat", "tab", "ant", "tan", "mat", "tame"};
-        System.out.println(GroupAnagrams.naiveSolution(str));
+        //System.out.println(GroupAnagrams.naiveSolution(str));
+        //System.out.println(GroupAnagrams.hashMapMethod(str));
+        System.out.println(GroupAnagrams.groupAnagrams(str));
 
+
+    }
+
+
+    /**
+     *
+     * counting frequency of str - no sorting
+     * Time complexity - O(n . K)
+     */
+    public static List<List<String>> groupAnagrams(String[] str) {
+        Map<String, List<String>> map = new HashMap<>();
+
+
+
+        for (String s : str) {
+            // count frequency
+            int[] count = new int[26];
+
+            for (char c  : s.toCharArray()) {
+                count[c - 'a']++ ; // i.e [E.G b = 98(ASCII) ] -- 98-97 = count[1]++
+            }
+
+            // build string from the frequency array
+            // # as a delimiter
+
+            StringBuilder builder = new StringBuilder();
+            for(int freq : count) {
+                builder.append(freq).append('#');
+            }
+            String key = builder.toString();
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+
+        }
+
+        return new ArrayList<>(map.values());
     }
 
     /**
      *
-     * Time complexity O(n^2 . K )
+     * Sort all strings once -
+     * create a key with its canonical form and add list of values as the original string
+     * Time complexity - O(n . k log k)
+     */
+    public static List<List<String>> hashMapMethod(String[] str) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s : str) {
+            // convert to array of char for sorting
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            /**
+             * if key exist insert the original string s
+             * if key doesn't exist create an empty list return and insert string s
+             */
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return  new ArrayList<>(map.values());
+    }
+
+    /**
+     *
+     * Time complexity O(n^2 . K log K )
      */
     public static List<List<String>> naiveSolution(String[] str) {
         int n = str.length;
